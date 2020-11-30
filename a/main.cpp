ae9ac024399d0e3540d41ac627e21a37b5d8f22e
Gleb Astashkin <gleb.astashkin@gmail.com>
2020-12-01 01:02:08 +0300

    Initial commit

---
+++ a/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <stdint.h>
+#include <string.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <functional>
+#include <bitset>
+#include <functional>
+#include <chrono>
+#include <random>
+
+typedef long long int i64;
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
