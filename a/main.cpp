82d61da67659071d267401fef4088ef8a67cdd98
Gleb Astashkin <gleb.astashkin@gmail.com>
2020-12-07 07:21:40 +0300

    day 6

--- a/main.cpp
+++ a/main.cpp
@@ -35,156 +35,39 @@ typedef long long int i64;
 
 const i64 inf = 1'000'000'000'000'000'000ll + 7;
 
-using TPassport = map<string, vector<string>>;
-
-i64 calcDigits(i64 v) {
-    i64 res = 0;
-    while (v) {
-        res += 1;
-        v /= 10;
-    }
-    return res;
-}
-
-bool validateInt(string val, i64 l, i64 r) {
-    i64 limit = calcDigits(l);
-    if (calcDigits(r) != limit) {
-        throw 1;
-    }
-
-    if (val.size() != limit) {
-        return false;
-    }
-    
-    for (auto c : val) {
-        if ('0' <= c && c <= '9') {
-            continue;
-        }
-        return false;
-    }
-
-    stringstream ss(val);
-    i64 v;
-    ss >> v;
-
-    return l <= v && v <= r;
-}
-
-bool validaiteHeight(string val) {
-    if (val.size() < 3) {
-        return false;
-    }
-
-    string num = val.substr(0, val.size() - 2);
-    string type = val.substr(val.size() - 2);
-    if (type == "cm") {
-        return validateInt(num, 150, 193);
-    }
-    if (type == "in") {
-        return validateInt(num, 59, 76);
-    }
-    return false;
-}
-
-bool validateHairColor(string val) {
-    if (val.size() != 7) {
-        return false;
-    }
-    if (val[0] != '#') {
-        return false;
-    }
-
-    for (i64 i = 1; i < 7; i++) {
-        if ('0' <= val[i] && val[i] <= '9') {
-            continue;
-        }
-        if ('a' <= val[i] && val[i] <= 'f') {
-            continue;
-        }
-        return false;
-    }
-    return true;
-}
-
-bool validateEyeColor(string val) {
-    set<string> available = { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-    return available.count(val);
-}
-
-bool validatePassportId(string val) {
-    if (val.size() != 9) {
-        return false;
-    }
-    for (auto c : val) {
-        if ('0' <= c && c <= '9') {
-            continue;
-        }
-        return false;
-    }
-    return true;
-}
-
-bool isValid(const TPassport& data) {
-    vector<string> required = {
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid",
-        // "cid",
-    };
-
-    for (auto key : required) {
-        if (!data.count(key)) {
-            return false;
-        }
-    }
-
-    bool res = true;
-
-    res &= validateInt(data.at("byr").front(), 1920, 2002);
-    res &= validateInt(data.at("iyr").front(), 2010, 2020);
-    res &= validateInt(data.at("eyr").front(), 2020, 2030);
-    res &= validaiteHeight(data.at("hgt").front());
-    res &= validateHairColor(data.at("hcl").front());
-    res &= validateEyeColor(data.at("ecl").front());
-    res &= validatePassportId(data.at("pid").front());
-
-    return res;
-}
 
 int main(int argc, char* argv[]) {
-    vector<TPassport> a;
-    a.push_back(TPassport());
+    i64 R = 0;
+    map<char, i64> a;
+    i64 c = 0;
 
     while (!cin.eof()) {
         string line;
         getline(cin, line);
 
         if (line.empty()) {
-            a.push_back(TPassport());
+            for (auto e : a) {
+                if (e.second != c) {
+                    continue;
+                }
+                R += 1;
+            }
+            a.clear();
+            c = 0;
             continue;
         }
 
-        stringstream ss(line);
-        while (!ss.eof()) {
-            string data;
-            ss >> data;
-            auto i = data.find(':');
-            string key = data.substr(0, i);
-            string val = data.substr(i + 1);
-
-            a.back()[key].push_back(val);
+        c += 1;
+        for (auto c : line) {
+            a[c] += 1;
         }
     }
-
-    i64 R = 0;
-    for (auto& data : a) {
-        R += isValid(data) ? 1 : 0;
+    for (auto e : a) {
+        if (e.second != c) {
+            continue;
+        }
+        R += 1;
     }
-
     cout << R << endl;
 
 
