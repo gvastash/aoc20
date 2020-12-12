b90280d8bad7658f3695e2d8c7cf5fdb6f52be90
Gleb Astashkin <gleb.astashkin@gmail.com>
2020-12-13 00:59:10 +0300

    day 12

--- a/main.cpp
+++ a/main.cpp
@@ -50,6 +50,115 @@ const i64 inf = 1'000'000'000'000'000'000ll;
 
 const long double eps = 1e-8;
 
+char turnLeft(char d) {
+    switch (d) {
+    case 'E':
+        return 'N';
+    case 'W':
+        return 'S';
+    case 'N':
+        return 'W';
+    case 'S':
+        return 'E';
+    }
+
+    throw 1;
+}
+
+char turnRight(char d) {
+    switch (d) {
+    case 'E':
+        return 'S';
+    case 'W':
+        return 'N';
+    case 'N':
+        return 'E';
+    case 'S':
+        return 'W';
+    }
+
+    throw 1;
+}
+
+pair<i64, i64> turnWaypointRight(pair<i64, i64> wp) {
+    return {-wp.second, wp.first};
+}
+
+pair<i64, i64> turnWaypointLeft(pair<i64, i64> wp) {
+    return { wp.second, -wp.first };
+}
+
+void handleMove(pair<i64, i64> e, i64& x, i64& y, char& d) {
+    switch (e.first) {
+    case 'N':
+        y -= e.second;
+        break;
+    case 'S':
+        y += e.second;
+        break;
+    case 'W':
+        x -= e.second;
+        break;
+    case 'E':
+        x += e.second;
+        break;
+    case 'L':
+        for (i64 i = 0; i < e.second; i += 90) {
+            d = turnLeft(d);
+        }
+        break;
+    case 'R':
+        for (i64 i = 0; i < e.second; i += 90) {
+            d = turnRight(d);
+        }
+        break;
+    case 'F':
+        handleMove({ d, e.second }, x, y, d);
+        break;
+    default:
+        throw 1;
+    }
+}
+
+
+void handleWaypointMove(pair<i64, i64> e, i64& x, i64& y, i64& wpx, i64& wpy) {
+    switch (e.first) {
+    case 'N':
+        wpy -= e.second;
+        break;
+    case 'S':
+        wpy += e.second;
+        break;
+    case 'W':
+        wpx -= e.second;
+        break;
+    case 'E':
+        wpx += e.second;
+        break;
+    case 'L':
+        for (i64 i = 0; i < e.second; i += 90) {
+            auto t = turnWaypointLeft({ wpx, wpy });
+            wpx = t.first;
+            wpy = t.second;
+        }
+        break;
+    case 'R':
+        for (i64 i = 0; i < e.second; i += 90) {
+            auto t = turnWaypointRight({ wpx, wpy });
+            wpx = t.first;
+            wpy = t.second;
+        }
+        break;
+    case 'F':
+        x += wpx * e.second;
+        y += wpy * e.second;
+        break;
+    default:
+        throw 1;
+    }
+
+}
+
 int main(int argc, char* argv[]) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(15); cout.setf(ios::fixed); cerr.precision(15); cerr.setf(ios::fixed);
 
@@ -57,7 +166,7 @@ int main(int argc, char* argv[]) {
         cerr << "i64 != long long int" << endl;
     }
 
-    vector<string> a;
+    vector<pair<char, i64>> a;
     while (!cin.eof()) {
         string line;
         getline(cin, line);
@@ -66,82 +175,33 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        a.push_back(line);
+        stringstream ss(line.substr(1));
+        i64 val = 0;
+        ss >> val;
+
+        a.push_back({line[0], val});
     }
 
-    i64 n = a.size();
-    i64 m = a.front().size();
-
-    while (true) {
-        auto b = a;
-        for (i64 i = 0; i < n; i++) {
-            for (i64 j = 0; j < m; j++) {
-                auto getOccuped = [&]() {
-                    i64 occuped = 0;
-                    for (i64 x = -1; x <= 1; x++) {
-                        for (i64 y = -1; y <= 1; y++) {
-                            if (x == 0 && y == 0) {
-                                continue;
-                            }
-                            for (i64 k = 1; true; k++) {
-                                i64 r = i + x * k;
-                                i64 c = j + y * k;
-                                if (0 > r || r >= n) {
-                                    break;
-                                }
-                                if (0 > c || c >= m) {
-                                    break;
-                                }
-                                if (a[r][c] == '.') {
-                                    continue;
-                                }
-
-                                if (a[r][c] == '#') {
-                                    occuped += 1;
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    return occuped;
-                };
-                switch (a[i][j]) {
-                    case '.':
-                        continue;
-                    case 'L': {
-                        if (!getOccuped()) {
-                            b[i][j] = '#';
-                        }
-                        break;
-                    }
-                    case '#': {
-                        if (getOccuped() >= 5) {
-                            b[i][j] = 'L';
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-        if (a == b) {
-            break;
-        }
-        a = b;
-    }
+    /*
+    i64 x = 0;
+    i64 y = 0;
+    char d = 'E';
 
     for (auto e : a) {
-        cerr << e << endl;
+        handleMove(e, x, y, d);
+        //cerr << x << " " << y << endl;
     }
+    */
 
-    i64 occuped = 0;
+    i64 x = 0;
+    i64 y = 0;
+    i64 wpx = 10;
+    i64 wpy = -1;
     for (auto e : a) {
-        for (auto c : e) {
-            occuped += (c == '#' ? 1 : 0);
-        }
+        handleWaypointMove(e, x, y, wpx, wpy);
     }
-    cout << occuped << endl;
-
-
+    i64 R = abs(x) + abs(y);
+    cout << R << endl;
 
     return 0;
 }
