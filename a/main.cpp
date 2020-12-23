0db45bc887dd8394392b7999083f87ed119edb87
Gleb Astashkin <gleb.astashkin@gmail.com>
2020-12-23 13:29:06 +0300

    day 22

--- a/main.cpp
+++ a/main.cpp
@@ -50,237 +50,45 @@ const i64 inf = 1'000'000'000'000'000'000ll;
 
 const long double eps = 1e-8;
 
-void solve() {
-    string s = "789465123";
-    char c = s.front();
+void Play(vector<vector<i64>>& q, vector<i64>& k) {
+    while (k[0] < q[0].size() && k[1] < q[1].size()) {
+        vector<i64> v = { q[0][k[0]], q[1][k[1]] };
+        if (k[0] + v[0] < q[0].size() && k[1] + v[1] < q[1].size()) {
+            vector<vector<i64>> nq(2);
+            vector<i64> nk(2);
 
-    for (i64 tt = 1; tt <= 100; tt++) {
-        stringstream qs;
-        for (i64 i = 0; i < s.size(); i++) {
-            if (s[i] == c) {
-                for (i64 j = 1; j <= 3; j++) {
-                    qs << s[(i + j) % s.size()];
-                }
-                break;
-            }
-        }
-        string q = qs.str();
-        char p = c - 1;
-        while (true) {
-            if (p == '0') {
-                p = '9';
-                continue;
-            }
-
-            bool ok = true;
-            for (auto t : q) {
-                if (t == p) {
-                    p -= 1;
-                    ok = false;
-                    break;
+            for (i64 j = 0; j < 2; j++) {
+                for (i64 i = k[j]; i < q[j].size(); i++) {
+                    nq[j].push_back(q[j][k[j]]);
                 }
             }
 
+            Play(nq, nk);
 
-            if (ok) {
-                break;
+            if (nk[0] < nq[0].size()) {
+                q[0].push_back(q[0][k[0]]);
+                q[0].push_back(q[1][k[1]]);
+            }
+            else {
+                q[1].push_back(q[1][k[1]]);
+                q[1].push_back(q[0][k[0]]);
+            }
+        }
+        else {
+            if (q[0][k[0]] > q[1][k[1]]) {
+                q[0].push_back(q[0][k[0]]);
+                q[0].push_back(q[1][k[1]]);
+            }
+            else {
+                q[1].push_back(q[1][k[1]]);
+                q[1].push_back(q[0][k[0]]);
             }
         }
 
-        stringstream ss;
-        for (i64 i = 0; i < s.size(); i++) {
-            bool ok = true;
-            for (i64 j = 0; j < q.size(); j++) {
-                if (q[j] == s[i]) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (!ok) {
-                continue;
-            }
-            ss << s[i];
-            if (s[i] == p) {
-                ss << q;
-            }
-        }
-
-        s = ss.str();
-        for (i64 i = 0; i < s.size(); i++) {
-            if (s[i] == c) {
-                c = s[(i + 1) % s.size()];
-                break;
-            }
-        }
-
-        cerr << s << endl;
-    }
-
-    for (i64 i = 0; i < s.size(); i++) {
-        if (s[i] == '1') {
-            stringstream ss;
-            for (i64 j = 1; j < s.size(); j++) {
-                ss << s[(i + j) % s.size()];
-            }
-            cout << ss.str() << endl;
-            break;
-        }
+        k[0] += 1;
+        k[1] += 1;
     }
 }
-
-void solve2() {
-    string s = "789465123";
-
-    const i64 m = 1'000'000; // s.size();
-
-    vector<i64> l(m + 1);
-    vector<i64> r(m + 1);
-
-    for (i64 i = 0; i < s.size(); i++) {
-        if (i > 0) {
-            l[(i64)s[i] - '0'] = (i64)s[i - 1] - '0';
-        }
-
-        if (i < s.size() - 1) {
-            r[(i64)s[i] - '0'] = (i64)s[i + 1] - '0';
-        }
-    }
-
-    for (i64 i = s.size() + 1; i <= m; i++) {
-        if (i > s.size()) {
-            l[i] = i - 1;
-        }
-        if (i < m) {
-            r[i] = i + 1;
-        }
-    }
-
-    if (m > s.size()) {
-        l[(i64)s.front() - '0'] = m;
-        r[m] = (i64)s.front() - '0';
-
-        r[(i64)s.back() - '0'] = s.size() + 1;
-        l[s.size() + 1] = (i64)s.back() - '0';
-    }
-    else {
-        l[(i64)s.front() - '0'] = (i64)s.back() - '0';
-        r[(i64)s.back() - '0'] = (i64)s.front() - '0';
-    }
-
-    i64 c = (i64)s.front() - '0';
-
-    if (false) {
-        i64 v = c;
-        do {
-            cerr << v << " ";
-            v = r[v];
-        } while (v != c);
-        cerr << endl;
-    }
-
-    if (false) {
-        i64 v = c;
-        do {
-            cerr << v << " ";
-            v = l[v];
-        } while (v != c);
-        cerr << endl;
-    }
-
-    for (i64 tt = 1; tt <= 10'000'000; tt++) {
-        set<i64> q;
-        i64 ql = 0;
-        i64 qr = 0;
-
-        if (false) {
-            i64 v = c;
-            do {
-                cerr << v << " ";
-                v = r[v];
-            } while (v != c);
-            cerr << endl;
-        }
-
-
-        i64 t = c;
-        for (i64 i = 0; i < 3; i++) {
-            t = r[t];
-            if (!ql) {
-                ql = t;
-            }
-            qr = t;
-            q.insert(t);
-        }
-
-        t = r[t];
-
-        r[c] = t;
-        l[t] = c;
-
-        if (false) {
-            i64 v = c;
-            do {
-                cerr << v << " ";
-                v = r[v];
-            } while (v != c);
-            cerr << endl;
-        }
-
-        //l[ql] = 0;
-        //r[qr] = 0;
-
-        i64 p = c - 1;
-        while (true) {
-            if (!p) {
-                p = m;
-                continue;
-            }
-
-            if (q.count(p)) {
-                p -= 1;
-                continue;
-            }
-
-            break;
-        }
-
-        i64 np = r[p];
-
-        r[p] = ql;
-        l[ql] = p;
-
-
-        l[np] = qr;
-        r[qr] = np;
-
-
-        if (false) {
-            i64 v = c;
-            do {
-                cerr << v << " ";
-                v = r[v];
-            } while (v != c);
-            cerr << endl;
-        }
-
-        c = r[c];
-    }
-
-    if (false) {
-        i64 v = c;
-        do {
-            cerr << v << " ";
-            v = r[v];
-        } while (v != c);
-        cerr << endl;
-    }
-
-    i64 v1 = r[1];
-    i64 v2 = r[r[1]];
-    cerr << v1 << " " << v2 << endl;
-    cout << v1 * v2 << endl;
-}
-
 
 int main(int argc, char* argv[]) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(15); cout.setf(ios::fixed); cerr.precision(15); cerr.setf(ios::fixed);
@@ -289,7 +97,43 @@ int main(int argc, char* argv[]) {
         cerr << "i64 != long long int" << endl;
     }
 
-    solve2();
+
+    vector<vector<i64>> q(2);
+
+    i64 cnt = 0;
+    while (!cin.eof()) {
+        string line;
+        getline(cin, line);
+
+        if (line.empty()) {
+            cnt += 1;
+            if (cnt > 1) {
+                break;
+            }
+            continue;
+        }
+
+        if (line.find("Player") != string::npos) {
+            continue;
+        }
+        stringstream ss(line);
+        i64 v = 0;
+        ss >> v;
+        q[cnt].push_back(v);
+    }
+
+    vector<i64> k(2);
+
+
+
+    i64 R = 0;
+    for (i64 j = 0; j < 2; j++) {
+        for (i64 i = k[j]; i < q[j].size(); i++) {
+            R += q[j][i] * (q[j].size() - i);
+        }
+    }
+
+    cout << R << endl;
 
     return 0;
 }
