099e7a5c6d04c99eabc473ead235a1d8542e3f5f
Gleb Astashkin <gleb.astashkin@gmail.com>
2020-12-23 13:27:52 +0300

    day 23

--- a/main.cpp
+++ a/main.cpp
@@ -50,18 +50,237 @@ const i64 inf = 1'000'000'000'000'000'000ll;
 
 const long double eps = 1e-8;
 
-set<string> Intersect(set<string>& a, set<string>& b) {
-    set<string> res;
+void solve() {
+    string s = "789465123";
+    char c = s.front();
 
-    for (auto e : a) {
-        if (!b.count(e)) {
-            continue;
+    for (i64 tt = 1; tt <= 100; tt++) {
+        stringstream qs;
+        for (i64 i = 0; i < s.size(); i++) {
+            if (s[i] == c) {
+                for (i64 j = 1; j <= 3; j++) {
+                    qs << s[(i + j) % s.size()];
+                }
+                break;
+            }
         }
-        res.insert(e);
+        string q = qs.str();
+        char p = c - 1;
+        while (true) {
+            if (p == '0') {
+                p = '9';
+                continue;
+            }
+
+            bool ok = true;
+            for (auto t : q) {
+                if (t == p) {
+                    p -= 1;
+                    ok = false;
+                    break;
+                }
+            }
+
+
+            if (ok) {
+                break;
+            }
+        }
+
+        stringstream ss;
+        for (i64 i = 0; i < s.size(); i++) {
+            bool ok = true;
+            for (i64 j = 0; j < q.size(); j++) {
+                if (q[j] == s[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (!ok) {
+                continue;
+            }
+            ss << s[i];
+            if (s[i] == p) {
+                ss << q;
+            }
+        }
+
+        s = ss.str();
+        for (i64 i = 0; i < s.size(); i++) {
+            if (s[i] == c) {
+                c = s[(i + 1) % s.size()];
+                break;
+            }
+        }
+
+        cerr << s << endl;
     }
 
-    return res;
+    for (i64 i = 0; i < s.size(); i++) {
+        if (s[i] == '1') {
+            stringstream ss;
+            for (i64 j = 1; j < s.size(); j++) {
+                ss << s[(i + j) % s.size()];
+            }
+            cout << ss.str() << endl;
+            break;
+        }
+    }
 }
+
+void solve2() {
+    string s = "789465123";
+
+    const i64 m = 1'000'000; // s.size();
+
+    vector<i64> l(m + 1);
+    vector<i64> r(m + 1);
+
+    for (i64 i = 0; i < s.size(); i++) {
+        if (i > 0) {
+            l[(i64)s[i] - '0'] = (i64)s[i - 1] - '0';
+        }
+
+        if (i < s.size() - 1) {
+            r[(i64)s[i] - '0'] = (i64)s[i + 1] - '0';
+        }
+    }
+
+    for (i64 i = s.size() + 1; i <= m; i++) {
+        if (i > s.size()) {
+            l[i] = i - 1;
+        }
+        if (i < m) {
+            r[i] = i + 1;
+        }
+    }
+
+    if (m > s.size()) {
+        l[(i64)s.front() - '0'] = m;
+        r[m] = (i64)s.front() - '0';
+
+        r[(i64)s.back() - '0'] = s.size() + 1;
+        l[s.size() + 1] = (i64)s.back() - '0';
+    }
+    else {
+        l[(i64)s.front() - '0'] = (i64)s.back() - '0';
+        r[(i64)s.back() - '0'] = (i64)s.front() - '0';
+    }
+
+    i64 c = (i64)s.front() - '0';
+
+    if (false) {
+        i64 v = c;
+        do {
+            cerr << v << " ";
+            v = r[v];
+        } while (v != c);
+        cerr << endl;
+    }
+
+    if (false) {
+        i64 v = c;
+        do {
+            cerr << v << " ";
+            v = l[v];
+        } while (v != c);
+        cerr << endl;
+    }
+
+    for (i64 tt = 1; tt <= 10'000'000; tt++) {
+        set<i64> q;
+        i64 ql = 0;
+        i64 qr = 0;
+
+        if (false) {
+            i64 v = c;
+            do {
+                cerr << v << " ";
+                v = r[v];
+            } while (v != c);
+            cerr << endl;
+        }
+
+
+        i64 t = c;
+        for (i64 i = 0; i < 3; i++) {
+            t = r[t];
+            if (!ql) {
+                ql = t;
+            }
+            qr = t;
+            q.insert(t);
+        }
+
+        t = r[t];
+
+        r[c] = t;
+        l[t] = c;
+
+        if (false) {
+            i64 v = c;
+            do {
+                cerr << v << " ";
+                v = r[v];
+            } while (v != c);
+            cerr << endl;
+        }
+
+        //l[ql] = 0;
+        //r[qr] = 0;
+
+        i64 p = c - 1;
+        while (true) {
+            if (!p) {
+                p = m;
+                continue;
+            }
+
+            if (q.count(p)) {
+                p -= 1;
+                continue;
+            }
+
+            break;
+        }
+
+        i64 np = r[p];
+
+        r[p] = ql;
+        l[ql] = p;
+
+
+        l[np] = qr;
+        r[qr] = np;
+
+
+        if (false) {
+            i64 v = c;
+            do {
+                cerr << v << " ";
+                v = r[v];
+            } while (v != c);
+            cerr << endl;
+        }
+
+        c = r[c];
+    }
+
+    if (false) {
+        i64 v = c;
+        do {
+            cerr << v << " ";
+            v = r[v];
+        } while (v != c);
+        cerr << endl;
+    }
+
+    i64 v1 = r[1];
+    i64 v2 = r[r[1]];
+    cerr << v1 << " " << v2 << endl;
+    cout << v1 * v2 << endl;
+}
+
 
 int main(int argc, char* argv[]) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(15); cout.setf(ios::fixed); cerr.precision(15); cerr.setf(ios::fixed);
@@ -70,155 +289,7 @@ int main(int argc, char* argv[]) {
         cerr << "i64 != long long int" << endl;
     }
 
-
-    map<set<string>, set<string>> q;
-
-    i64 tile = 0;
-    while (!cin.eof()) {
-        string line;
-        getline(cin, line);
-
-        if (line.empty()) {
-            break;
-        }
-
-        line.pop_back();
-        auto i = line.find('(');
-
-        stringstream nss(line.substr(0, i - 1));
-        stringstream gss(line.substr(i + 1));
-
-        set<string> names;
-        while (!nss.eof()) {
-            string name;
-            nss >> name;
-            names.insert(name);
-        }
-
-        set<string> gens;
-        while (!gss.eof()) {
-            string gen;
-            gss >> gen;
-            if (gen == "contains") {
-                continue;
-            }
-            if (gen.back() == ',') {
-                gen.pop_back();
-            }
-            gens.insert(gen);
-        }
-
-        q[names] = gens;
-    }
-
-    map<string, i64> allNames;
-    set<string> allGens;
-    for (auto& e : q) {
-        for (auto& name : e.first) {
-            allNames[name] += 1;
-        }
-        for (auto& gen : e.second) {
-            allGens.insert(gen);
-        }
-    }
-
-    cerr << allNames.size() << endl;
-    cerr << allGens.size() << endl;
-
-    map<string, string> tags;
-
-    //i64 T = 100;
-    //for (i64 tt = 1; tt <= T; tt++) {
-    while (tags.size() < allGens.size()) {
-        map<string, vector<set<string>>> z;
-        for (auto& e : q) {
-            for (auto gen : e.second) {
-                z[gen].push_back(e.first);
-            }
-        }
-
-
-        for (auto& e : z) {
-            while (e.second.size() != 1) {
-                auto r = Intersect(e.second.back(), e.second[e.second.size() - 2]);
-                e.second.pop_back();
-                e.second.pop_back();
-                e.second.push_back(r);
-            }
-
-            if (e.second.front().size() == 1) {
-                auto name = *e.second.front().begin();
-                if (tags.count(name) && tags[name] != e.first) {
-                    throw 1;
-                }
-                tags[name] = e.first;
-            }
-        }
-
-        map<set<string>, set<string>> nq;
-
-        for (auto& e : q) {
-            auto keys = e.first;
-            auto vals = e.second;
-
-            for (auto t : tags) {
-                if (!keys.count(t.first)) {
-                    continue;
-                }
-                keys.erase(t.first);
-                if (t.second.empty()) {
-                    continue;
-                }
-                vals.erase(t.second);
-            }
-
-            nq[keys] = vals;
-
-            /*
-
-            if (!vals.empty()) {
-                continue;
-            }
-
-            for (auto& name : keys) {
-                if (tags.count(name) && tags[name] != "") {
-                    throw 1;
-                }
-                tags[name] = "";
-            }
-
-            */
-        }
-
-        q = nq;
-    }
-
-    /*
-    i64 R = 0;
-    for (auto& e : allNames) {
-        if (tags.count(e.first)) {
-            continue;
-        }
-        R += e.second;
-    }
-    cout << R << endl;
-    */
-
-    vector<pair<string, string>> w;
-    for (auto& e : tags) {
-        w.push_back({ e.second, e.first });
-    }
-    sort(w.begin(), w.end());
-
-    stringstream ss;
-    for (i64 i = 0; i < w.size(); i++) {
-        if (i > 0) {
-            ss << ",";
-        }
-        ss << w[i].second;
-    }
-
-    cout << ss.str() << endl;
+    solve2();
 
     return 0;
 }
